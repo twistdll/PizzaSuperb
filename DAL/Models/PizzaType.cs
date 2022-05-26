aaaf74cdc25f1142083214da96c2c15541a80056
twistballz <f.lala02@mail.ru>
2022-05-26 20:34:00 +0300

    DAL initial commit

---
+++ DAL/Models/PizzaType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+    internal class PizzaType
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int? Discount { get; set; }
+    }
+}
