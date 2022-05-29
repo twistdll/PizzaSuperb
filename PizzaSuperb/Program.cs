cf40319e27f4eefd43fcf6f842090fb65335791c
twistballz <f.lala02@mail.ru>
2022-05-29 16:58:19 +0300

    gitignore fix

---
+++ PizzaSuperb/Program.cs
@@ -0,0 +1,28 @@
+using BLL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddControllersWithViews();
+services.AddBLL(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
